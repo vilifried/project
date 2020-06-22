244a48ef7286e565ccebd7878a88ff6dc2a27471
Wilfried Haderer <w.haderer@gmx.net>
2020-06-23 00:38:35 +0200

    edit

--- src/app/update-catfact/update-catfact.page.ts
+++ src/app/update-catfact/update-catfact.page.ts
@@ -9,14 +9,25 @@ import {LocalStorageService} from '../services/local-storage.service';
 })
 export class UpdateCatfactPage implements OnInit {
 
+    localStorage: LocalStorageService;
     itemId: number;
+    catfactTitle: string;
+    catfactText: string;
+    catImage: any;
 
     constructor(private route: ActivatedRoute,
                 private router: Router,
                 private localStorageService: LocalStorageService) {
         if (this.route.snapshot.paramMap.get('itemId')) {
             this.itemId = Number(this.route.snapshot.paramMap.get('itemId'));
+            this.localStorage = localStorageService;
         }
+    }
+
+    loadSingleCatfact() {
+        this.catImage = this.localStorage.myCatfactList[this.itemId].img;
+        this.catfactText = this.localStorage.myCatfactList[this.itemId].catfact;
+        this.catfactTitle = this.localStorage.myCatfactList[this.itemId].title;
     }
 
     navigateToViewCatfact(parameter: number) {
@@ -24,6 +35,8 @@ export class UpdateCatfactPage implements OnInit {
     }
 
     ngOnInit() {
+        this.localStorage.readStorage().then((value) => {
+            this.loadSingleCatfact();
+        });
     }
-
 }
